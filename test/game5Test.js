511a3c4fd512f010b77cf4d677ac3fae94022d87
Jinja <prajwal.kb.1999@gmail.com>
2023-02-09 22:07:34 +0530

    challenge 5 done

--- test/game5Test.js
+++ test/game5Test.js
@@ -11,9 +11,22 @@ describe('Game5', function () {
   it('should be a winner', async function () {
     const { game } = await loadFixture(deployContractAndSetVariables);
 
-    // good luck
+    const threshold = "0x00FfFFfFFFfFFFFFfFfFfffFFFfffFfFffFfFFFf";
+    let wallet = ethers.Wallet.createRandom();
+    while ((await wallet.getAddress()) >= threshold) {
+      wallet = ethers.Wallet.createRandom();
+    }
 
-    await game.win();
+    const signer = ethers.provider.getSigner(0);
+    await signer.sendTransaction({
+      to: await wallet.getAddress(),
+      value: ethers.utils.parseEther("10")
+    })
+
+    const providerWallet = wallet.connect(ethers.provider);
+    await game.connect(providerWallet).win();
+
+    // good luck
 
     // leave this assertion as-is
     assert(await game.isWon(), 'You did not win the game');
