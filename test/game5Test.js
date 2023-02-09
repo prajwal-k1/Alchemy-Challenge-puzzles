3cee0d9259e351b7ae8432e68201c96b92dcb2ce
Jinja <prajwal.kb.1999@gmail.com>
2023-02-09 22:12:03 +0530

    added few comments

--- test/game5Test.js
+++ test/game5Test.js
@@ -12,18 +12,18 @@ describe('Game5', function () {
     const { game } = await loadFixture(deployContractAndSetVariables);
 
     const threshold = "0x00FfFFfFFFfFFFFFfFfFfffFFFfffFfFffFfFFFf";
-    let wallet = ethers.Wallet.createRandom();
-    while ((await wallet.getAddress()) >= threshold) {
+    let wallet = ethers.Wallet.createRandom();//create a random address
+    while ((await wallet.getAddress()) >= threshold) { //loop until we find random address >= threshold
       wallet = ethers.Wallet.createRandom();
     }
 
     const signer = ethers.provider.getSigner(0);
-    await signer.sendTransaction({
+    await signer.sendTransaction({ //send some ETH for gas
       to: await wallet.getAddress(),
       value: ethers.utils.parseEther("10")
     })
 
-    const providerWallet = wallet.connect(ethers.provider);
+    const providerWallet = wallet.connect(ethers.provider);//connecting to provider like metamask or something, here we use default ethers provider
     await game.connect(providerWallet).win();
 
     // good luck
